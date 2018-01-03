c4a0b9b5bc97ceda66db4b3f25432b0050abd84d
19648 <1964855183@qq.com>
2018-01-03 18:05:45 +0800

    Dingo API 初探

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,14 @@
 |
 */
 
-Route::group(['prefix'=>'api/v1'], function () {
-   Route::resource('lessons','LessonsController');
+//Route::group(['prefix'=>'api/v1'], function () {
+//   Route::resource('lessons','LessonsController');
+//});
+
+$api = app('Dingo\Api\Routing\Router');
+$api->version('v1', function ($api) {
+   $api->group(['namespace' => 'App\Api\Controllers'], function ($api) {
+      $api->get('lessons', 'LessonsController@index');
+      $api->get('lessons/{id}', 'LessonsController@show');
+   });
 });
